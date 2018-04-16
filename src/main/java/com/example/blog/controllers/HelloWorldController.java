84e18b8b375b927422e2b231231089dc8615dc0b
fer <fernandoza@gmail.com>
2018-04-16 16:03:33 -0500

    Add comments and better organized code, improved output

--- src/main/java/com/example/blog/controllers/HelloWorldController.java
+++ src/main/java/com/example/blog/controllers/HelloWorldController.java
@@ -6,28 +6,38 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("/hello/{name}/{lastname}")
+    // This is the simplest way to test a get request from the controller
+    @GetMapping("/hello")
+    // ResponseBody makes sure you always get plain text in return and displayed in the browser
     @ResponseBody
-    public String sayHello(@PathVariable String name, @PathVariable String lastname){
-        return  "Hello World, " + name + " " + lastname;
+    public String sayHello(){
+        return  "Hello World";
     }
 
+    // This is how you get access to the url params like: /hello/fer
     @GetMapping("/hello/{name}")
     @ResponseBody
-    public String sayHelloFN(@PathVariable String name){
-        return  "Hello World, " + name ;
+    public String sayHelloF(@PathVariable String name){
+        return  "Hello " + name + "!" ;
     }
 
-    @GetMapping("/logout")
+    @GetMapping("/hello/{name}/{lastname}")
+    @ResponseBody
+    public String sayHelloFL(@PathVariable String name, @PathVariable String lastname){
+        return  "Hello " + name + " " + lastname;
+    }
+
+    @GetMapping("/logmeout")
     @ResponseBody
     public String logout(){
         return "you have been logged out";
     }
 
-    @RequestMapping(path = "/lights", method = RequestMethod.GET)
+    // This is another way to create a method that listen for a get request
+    @RequestMapping(path = "/lights/{mode}", method = RequestMethod.GET)
     @ResponseBody
-    public String lights(){
-        return "Lights on";
+    public String lights(@PathVariable String mode){
+        return "Lights " + mode;
     }
 
 }
