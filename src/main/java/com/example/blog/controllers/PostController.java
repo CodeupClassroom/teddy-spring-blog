708d517cdd274bde835ff5089a39e5ceae3b3020
Justin Reich <justinreich.dev@gmail.com>
2018-04-18 12:18:16 -0500

    Add service exercise solution

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.example.blog.controllers;
 
 
 import com.example.blog.models.Post;
+import com.example.blog.services.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,24 +16,21 @@ import java.util.List;
 @Controller
 public class PostController {
 
+    PostService postSvc;
+
+    public PostController(PostService postSvc) {
+        this.postSvc = postSvc;
+    }
+
     @GetMapping("/posts")
     public String index(Model model) {
-
-        List<Post> posts = new ArrayList<>();
-
-        posts.add(new Post("Post Title 1", "This is a description of post 1."));
-        posts.add(new Post("Post Title 2", "This is a description of post 2."));
-        posts.add(new Post("Post Title 3", "This is a description of post 3."));
-        posts.add(new Post("Post Title 4", "This is a description of post 4."));
-
-        model.addAttribute("posts", posts);
+        model.addAttribute("posts", postSvc.getAllPosts());
         return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String show(@PathVariable long id, Model model) {
-        Post post = new Post("Test Post", "This is a test description.");
-        model.addAttribute("post", post);
+        model.addAttribute("post", postSvc.getPost(id));
         return "/posts/show";
     }
 
