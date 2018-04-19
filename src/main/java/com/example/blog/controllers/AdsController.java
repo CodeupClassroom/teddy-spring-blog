5df0a21f522c3cdeae805d5eb295ec04a711c716
fer <fernandoza@gmail.com>
2018-04-19 16:37:47 -0500

    Finish the JPA exercise with Ads

--- src/main/java/com/example/blog/controllers/AdsController.java
+++ src/main/java/com/example/blog/controllers/AdsController.java
@@ -10,11 +10,9 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class AdsController {
 
-    private final AdService adSvc;
     private final AdRepository adDao;
 
-    public AdsController(AdService adSvc, AdRepository adDao) {
-        this.adSvc = adSvc;
+    public AdsController(AdRepository adDao) {
         this.adDao = adDao;
     }
 
@@ -26,7 +24,7 @@ public class AdsController {
 
     @GetMapping("/ads/{id}")
     public String show(@PathVariable long id, Model model) {
-        model.addAttribute("ad", adDao.findById(id));
+        model.addAttribute("ad", adDao.findOne(id));
         return "/ads/show";
     }
 
@@ -44,15 +42,19 @@ public class AdsController {
 
     @GetMapping("/ads/{id}/edit")
     public String edit(@PathVariable long id, Model viewModel){
-        viewModel.addAttribute("ad", adSvc.getAd(id));
+        viewModel.addAttribute("ad", adDao.findOne(id));
         return "/ads/edit";
     }
 
     @PostMapping("/ads/edit")
     public String handleEdit(@ModelAttribute Ad ad){
-        System.out.println("ad = " + ad.getId());
-        System.out.println("ad = " + ad.getDescription());
-        System.out.println("ad = " + ad.getTitle());
+        adDao.save(ad);
+        return "redirect:/ads";
+    }
+
+    @PostMapping("/ads/delete")
+    public String deleteAd(@ModelAttribute Ad ad){
+        adDao.delete(ad);
         return "redirect:/ads";
     }
 }
