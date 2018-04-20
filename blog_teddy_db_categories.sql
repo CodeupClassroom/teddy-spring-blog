32c76b4799d4c82ed0230725072b9d5827e5fa7a
fer <fernandoza@gmail.com>
2018-04-20 15:06:02 -0500

    Add a seeder for categories

---
+++ blog_teddy_db_categories.sql
@@ -0,0 +1,7 @@
+use blog_teddy_db;
+
+INSERT INTO categories (id, name) VALUES (1, 'Tech');
+INSERT INTO categories (id, name) VALUES (2, 'Art');
+INSERT INTO categories (id, name) VALUES (3, 'Films');
+INSERT INTO categories (id, name) VALUES (4, 'Music');
+INSERT INTO categories (id, name) VALUES (4, 'Music');
