9715a1b8e2c948ff75fad01169f63731d77f2bf1
fer <fernandoza@gmail.com>
2018-04-20 15:28:10 -0500

    Update the seeder for the whole DB examples

--- blog_teddy_db_categories.sql
+++
@@ -1,7 +0,0 @@
-use blog_teddy_db;
-
-INSERT INTO categories (id, name) VALUES (1, 'Tech');
-INSERT INTO categories (id, name) VALUES (2, 'Art');
-INSERT INTO categories (id, name) VALUES (3, 'Films');
-INSERT INTO categories (id, name) VALUES (4, 'Music');
-INSERT INTO categories (id, name) VALUES (4, 'Music');
